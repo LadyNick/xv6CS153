a2212a1be26e7a5e1025e14ac658746abba3a983
Nicole Navarro <nnava026@sledge.cs.ucr.edu>
2021-10-18 12:33:21 -0700

    Removing the S from exitS so the name stays the same

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int exitstat;		       //EDIT HERE LAB 1: THIS WILL HOLD THE EXIT STATUS
 };
 
 // Process memory is laid out contiguously, low addresses first:
