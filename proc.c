8b3386e3d3cb7fb0527cac2a7aceefb3d073fad6
LadyNick <72218995+LadyNick@users.noreply.github.com>
2021-11-03 15:23:10 -0700

    Update proc.c

--- proc.c
+++ proc.c
@@ -483,9 +483,9 @@ scheduler(void)
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
+      if(p->state != RUNNABLE){
         continue;
-
+      }
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
@@ -508,10 +508,8 @@ scheduler(void)
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
-    }\
+    }
     release(&ptable.lock);
-
-  }
 } 
 
 // Enter scheduler.  Must hold only ptable.lock
